6b43b706b3568443bae9a4ac5fa5262f78c3d074
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2026-06-08 00:06:28 -0600

    feat/ se implementaron mejoras de seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://oneoutcr.net',
   adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
